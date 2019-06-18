1f068e3d5005b3519d74c00c465c33c6de1e4235
runoob <mymrblack@live.com>
2019-06-18 21:11:51 +0800

    Added Zhiming's Code

---
+++ README.md
@@ -0,0 +1,103 @@
+# YOLO ROS: Multiple_Camera_Object_Tracking_With_Darknet_ROS
+
+## Overview
+
+This project is aiming to do objects tracking by using multiple cameras. The codes have not finished yet.
+
+## Citation
+
+The YOLO methods used in this software are described in the paper: [You Only Look Once: Unified, Real-Time Object Detection](https://arxiv.org/abs/1506.02640).
+
+This program was modified based on the following project.
+
+M. Bjelonic
+**"YOLO ROS: Real-Time Object Detection for ROS"**,
+URL: https://github.com/leggedrobotics/darknet_ros, 2018.
+
+    @misc{bjelonicYolo2018,
+      author = {Marko Bjelonic},
+      title = {{YOLO ROS}: Real-Time Object Detection for {ROS}},
+      howpublished = {\url{https://github.com/leggedrobotics/darknet_ros}},
+      year = {2016--2018},
+    }
+
+## Installation
+
+### Dependencies
+
+This software is built on the Robotic Operating System ([ROS]), which needs to be [installed](http://wiki.ros.org) first. Additionally, YOLO for ROS depends on following software:
+
+- [OpenCV](http://opencv.org/) (computer vision library),
+- [boost](http://www.boost.org/) (c++ library),
+
+### Building
+
+[![Build Status](https://ci.leggedrobotics.com/buildStatus/icon?job=github_leggedrobotics/darknet_ros/master)](https://ci.leggedrobotics.com/job/github_leggedrobotics/job/darknet_ros/job/master/)
+
+In order to install darknet_ros, clone the latest version using SSH (see [how to set up an SSH key](https://confluence.atlassian.com/bitbucket/set-up-an-ssh-key-728138079.html)) from this repository into your catkin workspace and compile the package using ROS.
+
+    cd catkin_workspace/src
+    git clone --recursive git@github.com:leggedrobotics/darknet_ros.git
+    cd ../
+
+To maximize performance, make sure to build in *Release* mode. You can specify the build type by setting
+
+    catkin_make -DCMAKE_BUILD_TYPE=Release
+
+or using the [Catkin Command Line Tools](http://catkin-tools.readthedocs.io/en/latest/index.html#)
+
+    catkin build darknet_ros -DCMAKE_BUILD_TYPE=Release
+
+Darknet on the CPU is fast (approximately 1.5 seconds on an Intel Core i7-6700HQ CPU @ 2.60GHz × 8) but it's like 500 times faster on GPU! You'll have to have an Nvidia GPU and you'll have to install CUDA. The CMakeLists.txt file automatically detects if you have CUDA installed or not. CUDA is a parallel computing platform and application programming interface (API) model created by Nvidia. If you do not have CUDA on your System the build process will switch to the CPU version of YOLO. If you are compiling with CUDA, you might receive the following build error:
+
+    nvcc fatal : Unsupported gpu architecture 'compute_61'.
+
+This means that you need to check the compute capability (version) of your GPU. You can find a list of supported GPUs in CUDA here: [CUDA - WIKIPEDIA](https://en.wikipedia.org/wiki/CUDA#Supported_GPUs). Simply find the compute capability of your GPU and add it into darknet_ros/CMakeLists.txt. Simply add a similar line like
+
+    -O3 -gencode arch=compute_62,code=sm_62
+
+### Download weights
+
+The yolo-voc.weights and tiny-yolo-voc.weights are downloaded automatically in the CMakeLists.txt file. If you need to download them again, go into the weights folder and download the two pre-trained weights from the COCO data set:
+
+    cd catkin_workspace/src/darknet_ros/darknet_ros/yolo_network_config/weights/
+    wget http://pjreddie.com/media/files/yolov2.weights
+    wget http://pjreddie.com/media/files/yolov2-tiny.weights
+
+And weights from the VOC data set can be found here:
+
+    wget http://pjreddie.com/media/files/yolov2-voc.weights
+    wget http://pjreddie.com/media/files/yolov2-tiny-voc.weights
+
+And the pre-trained weight from YOLO v3 can be found here:
+
+    wget http://pjreddie.com/media/files/yolov3-voc.weights
+    wget http://pjreddie.com/media/files/yolov3.weights
+
+### Use your own detection objects
+
+In order to use your own detection objects you need to provide your weights and your cfg file inside the directories:
+
+    catkin_workspace/src/darknet_ros/darknet_ros/yolo_network_config/weights/
+    catkin_workspace/src/darknet_ros/darknet_ros/yolo_network_config/cfg/
+
+In addition, you need to create your config file for ROS where you define the names of the detection objects. You need to include it inside:
+
+    catkin_workspace/src/darknet_ros/darknet_ros/config/
+
+Then in the launch file you have to point to your new config file in the line:
+
+    <rosparam command="load" ns="darknet_ros" file="$(find darknet_ros)/config/your_config_file.yaml"/>
+
+### Unit Tests
+
+Run the unit tests using the [Catkin Command Line Tools](http://catkin-tools.readthedocs.io/en/latest/index.html#)
+
+    catkin build darknet_ros --no-deps --verbose --catkin-make-args run_tests
+
+You will see the image above popping up.
+
+## Basic Usage
+
+In order to get YOLO ROS: Real-Time Object Detection for ROS to run with your robot, you will need to adapt a few parameters. It is the easiest if duplicate and adapt all the parameter files that you need to change from the `darkned_ros` package. These are specifically the parameter files in `config` and the launch file from the `launch` folder.
+
---
+++ darknet_ros/include/darknet_ros/YoloObjectDetector.hpp
@@ -0,0 +1,355 @@
+/*
+ * YoloObjectDetector.h
+ *
+ *  Created on: Dec 19, 2016
+ *      Author: Marko Bjelonic
+ *   Institute: ETH Zurich, Robotic Systems Lab
+ */
+
+#pragma once
+
+// c++
+#include <math.h>
+#include <string>
+#include <vector>
+#include <iostream>
+#include <pthread.h>
+#include <thread>
+#include <chrono>
+#include <mutex>
+#include <condition_variable>
+
+// ROS
+#include <ros/ros.h>
+#include <std_msgs/Header.h>
+#include <std_msgs/Int8.h>
+#include <actionlib/server/simple_action_server.h>
+#include <sensor_msgs/image_encodings.h>
+#include <sensor_msgs/Image.h>
+#include <geometry_msgs/Point.h>
+#include <image_transport/image_transport.h>
+
+// OpenCv
+#include <opencv2/imgproc/imgproc.hpp>
+#include <opencv2/highgui/highgui.hpp>
+#include <opencv2/objdetect/objdetect.hpp>
+#include <cv_bridge/cv_bridge.h>
+
+// darknet_ros_msgs
+#include <darknet_ros_msgs/BoundingBoxes.h>
+#include <darknet_ros_msgs/BoundingBox.h>
+#include <darknet_ros_msgs/CheckForObjectsAction.h>
+
+#include <darknet_ros_msgs/CorBBox.h>
+// Darknet.
+#ifdef GPU
+#include "cuda_runtime.h"
+#include "curand.h"
+#include "cublas_v2.h"
+#endif
+
+extern "C" {
+#include "network.h"
+#include "detection_layer.h"
+#include "region_layer.h"
+#include "cost_layer.h"
+#include "utils.h"
+#include "parser.h"
+#include "box.h"
+#include "darknet_ros/image_interface.h"
+#include <sys/time.h>
+}
+
+extern "C" void ipl_into_image(IplImage* src, image im);
+extern "C" image ipl_to_image(IplImage* src);
+extern "C" void show_image_cv(image p, const char *name, IplImage *disp);
+extern "C" void rgbgr_image(image im);
+
+
+//-------------------------Added By Lynne Begin -----------------
+#define CAMERA_NUM 2 //defined by lynne.
+#define SUBCRIBE_CAM_TOPIC_PATH_ROOT "subscribers/camera_reading_"
+#define CAMERA_TOPIC_DEFAULT_NAME "/camera/image_raw_"
+#define PUBLISH_OBJECT_DETECTOR_PATH_ROOT "publishers/object_detector_"
+#define OBJECT_DETECTOR_DEFAULT_NAME "found_object_"
+#define PUBLISH_BOUNDING_BOXES_PATH_ROOT "publishers/bounding_boxes_"
+#define BOUNDIGN_BOXES_DEFAULT_NAME "bounding_boxes_"
+#define PUBLISH_DETECTION_IMAGE_PATH_ROOT "publishers/detection_image_"
+#define DETECTION_IMAGE_DEFAULT_NAME "detection_image_"
+#define THRESHOLD_PATH "yolo_model/threshold/value"
+#define WEIGHTS_MODEL_PATH  "yolo_model/weight_file/name"
+#define DEFAULT_WEIGHT_FILE_NAME "yolov3-tiny.weights"
+typedef struct{
+
+  std::string cameraTopicNames[CAMERA_NUM]; //Added by Lynne.
+  int cameraQueueSize;
+
+  std::string objectDetectorTopicNames[CAMERA_NUM];
+  int objectDetectorQueueSize;
+  bool objectDetectorLatch;
+
+  std::string boundingBoxesTopicNames[CAMERA_NUM];
+  int boundingBoxesQueueSize;
+  bool boundingBoxesLatch;
+
+  std::string detectionImageTopicNames[CAMERA_NUM];
+  int detectionImageQueueSize;
+  bool detectionImageLatch;
+
+
+} TopicInputParams_;
+
+/*
+typedef struct{
+
+  ros::Publisher objectPublisher_[CAMERA_NUM];
+  ros::Publisher boundingBoxesPublisher_[CAMERA_NUM];
+
+  //! Publisher of the bounding box image.
+  ros::Publisher detectionImagePublisher_[CAMERA_NUM];
+
+} Publishers_;
+
+Publishers_ publishers_;
+*/
+//-------------------------Added By Lynne End-----------------
+
+namespace darknet_ros {
+
+//! Bounding box of the detected object.
+typedef struct
+{
+  float x, y, w, h, prob;
+  int num, Class;
+} RosBox_;
+
+/*************************************** New Added *********************************************************/
+
+// camera D415 intrincics
+const double x0 = 315.297; // principle point
+const double y0 = 248.041;
+const double fx = 615.094; // focal length
+const double fy = 614.703;
+
+//int ObjectCount = 0; // count for bounding boxes
+/***********************************************************************************************************/
+typedef struct
+{
+  IplImage* image;
+  std_msgs::Header header;
+} IplImageWithHeader_;
+
+class YoloObjectDetector
+{
+ public:
+  /*!
+   * Constructor.
+   */
+  explicit YoloObjectDetector(ros::NodeHandle nh);
+
+  /*!
+   * Destructor.
+   */
+  ~YoloObjectDetector();
+
+ private:
+  /*!
+   * Reads and verifies the ROS parameters.
+   * @return true if successful.
+   */
+  bool readParameters();
+
+  /*!
+   * Initialize the ROS connections.
+   */
+  void init();
+
+  /*!
+   * Callback of camera.
+   * @param[in] msg image pointer.
+   */
+  void cameraCallback(const sensor_msgs::ImageConstPtr& msg);
+
+/************************************** New Added *********************************************************/
+
+  //! callback function for subscriber of depth image
+  void depth_image_Callback(const sensor_msgs::ImageConstPtr& msg);
+
+/***********************************************************************************************************/
+  /*!
+   * Check for objects action goal callback.
+   */
+  void checkForObjectsActionGoalCB();
+
+  /*!
+   * Check for objects action preempt callback.
+   */
+  void checkForObjectsActionPreemptCB();
+
+  /*!
+   * Check if a preempt for the check for objects action has been requested.
+   * @return false if preempt has been requested or inactive.
+   */
+  bool isCheckingForObjects() const;
+
+  /*!
+   * Publishes the detection image.
+   * @return true if successful.
+   */
+  bool publishDetectionImage(const cv::Mat& detectionImage);
+
+  //! Typedefs.
+  typedef actionlib::SimpleActionServer<darknet_ros_msgs::CheckForObjectsAction> CheckForObjectsActionServer;
+  typedef std::shared_ptr<CheckForObjectsActionServer> CheckForObjectsActionServerPtr;
+
+  //! ROS node handle.
+  ros::NodeHandle nodeHandle_;
+
+  //! Class labels.
+  int numClasses_;
+  std::vector<std::string> classLabels_;
+
+  //! Check for objects action server.
+  CheckForObjectsActionServerPtr checkForObjectsActionServer_;
+
+  //! Advertise and subscribe to image topics.
+  image_transport::ImageTransport imageTransport_;
+
+  //! ROS subscriber and publisher.
+  image_transport::Subscriber imageSubscriber_[CAMERA_NUM];
+
+  //! Detected objects.
+  std::vector<std::vector<RosBox_> > rosBoxes_;
+  std::vector<int> rosBoxCounter_;
+  //darknet_ros_msgs::BoundingBoxes boundingBoxesResults_[CAMERA_NUM];
+  darknet_ros_msgs::BoundingBoxes boundingBoxesResults_;
+
+  //! Camera related parameters.
+  int frameWidth_;
+  int frameHeight_;
+
+/******************************** New Added Global Variables ************************************************/
+
+  //SortObject sortobject;
+  cv::Mat depth_image; // store depth image matrix
+  //! Subscriber of the depth image
+  ros::Subscriber depth_image_sub;
+  //! Publisher of the bounding box with (x,y,z)
+  ros::Publisher cor_depth_image_pub;
+
+/***********************************************************************************************************/
+  ros::Publisher objectPublisher_[CAMERA_NUM];
+  ros::Publisher boundingBoxesPublisher_[CAMERA_NUM];
+
+  //! Publisher of the bounding box image.
+  ros::Publisher detectionImagePublisher_[CAMERA_NUM];
+
+
+  // Yolo running on thread.
+  std::thread yoloThread_;
+
+  // Darknet.
+  char **demoNames_;
+  image **demoAlphabet_;
+  int demoClasses_;
+
+  network *net_;
+  std_msgs::Header headerBuff_[3];
+  //std_msgs::Header linkedHeaderBuff_[3];
+  image buff_[3];
+  image buffLetter_[3];
+  int buffId_[3];
+  int buffIndex_ = 0;
+  IplImage * ipl_;
+  float fps_ = 0;
+  float demoThresh_ = 0;
+  float demoHier_ = .5;
+  int running_ = 0;
+
+  int demoDelay_ = 0;
+  int demoFrame_ = 3;
+  float **predictions_;
+  int demoIndex_ = 0;
+  int demoDone_ = 0;
+  float *lastAvg2_;
+  float *lastAvg_;
+  float *avg_;
+  int demoTotal_ = 0;
+  double demoTime_;
+
+  RosBox_ *roiBoxes_;
+  bool viewImage_;
+  bool enableConsoleOutput_;
+  int waitKeyDelay_;
+  int fullScreen_;
+  char *demoPrefix_;
+
+  std_msgs::Header imageHeader_;
+  cv::Mat camImageCopy_;
+  boost::shared_mutex mutexImageCallback_;
+
+  bool imageStatus_ = false;
+  boost::shared_mutex mutexImageStatus_;
+
+  bool isNodeRunning_ = true;
+  boost::shared_mutex mutexNodeStatus_;
+
+  int actionId_;
+  boost::shared_mutex mutexActionStatus_;
+
+  // double getWallTime();
+
+  int sizeNetwork(network *net);
+
+  void rememberNetwork(network *net);
+
+  detection *avgPredictions(network *net, int *nboxes);
+
+  void *detectInThread();
+
+  void *fetchInThread();
+
+  void *displayInThread(void *ptr);
+
+  void *displayLoop(void *ptr);
+
+  void *detectLoop(void *ptr);
+
+  void setupNetwork(char *cfgfile, char *weightfile, char *datafile, float thresh,
+                    char **names, int classes,
+                    int delay, char *prefix, int avg_frames, float hier, int w, int h,
+                    int frames, int fullscreen);
+
+  void yolo();
+
+  IplImageWithHeader_ getIplImageWithHeader();
+
+  bool getImageStatus(void);
+
+  bool isNodeRunning(void);
+
+  void *publishInThread();
+
+//get camera topic names. Add by Lynne.
+//void getCameraTopic(std::string cameraTopicNames[]);
+  
+  void getTopicNameParameters(const std::string pathRoot, std::string topicNames[], int cam_num, const std::string defaultName);
+  
+  void getTopicInputParams(TopicInputParams_ *topicInputParams);
+  void publishTopics(TopicInputParams_ *topicInputParams);
+  
+  bool newImageComeFlag_ = false;
+  std::mutex mutexNewImageCome;
+  std::condition_variable newImageComeCondition;
+
+  std::string depthTopicName;
+  int depthQueueSize;
+
+  std::string corbboxTopicName;
+  int corbboxQueueSize;
+  bool corbboxLatch;
+
+  void imagePreprocess(image p, IplImage *disp);
+};
+
+} /* namespace darknet_ros*/
